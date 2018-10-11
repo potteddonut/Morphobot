5e32a4e32b3d96d0957b3f18ca62d00123116701
Morphoxeris <quartzcraftyt@gmail.com>
2018-10-12 00:15:26 +0800

    unfinished mylevel command

---
+++ commands/Moderation/mylevel.js
@@ -0,0 +1,34 @@
+const {
+    Command
+} = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            aliases: ['rank'],
+            permissionLevel: 0,
+            guarded: true,
+            description: "Displays your permission level in the message guild."
+        });
+    }
+
+    async run(message) {
+        let perm;
+        for (const level = 10; level >= 0; i--) {
+            if (!await message.hasAtLeastPermissionLevel(level)) continue;
+            perm = level;
+            break;
+        }
+
+        const levels = {
+            0: "Morphobot User",
+            5: "Server Moderator",
+            6: "Server Administrator",
+            7: "Server Owner",
+            8: "Morphobot Staff",
+            9: "Morphobot Administrator",
+            10: "Morphobot Developer"
+        }
+    }
+};
