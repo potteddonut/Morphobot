36fe437ab46ce225b1a609c03e244cd83882ed55
Morphoxeris <quartzcraftyt@gmail.com>
2018-10-12 22:25:47 +0800

    unfinished avatar command because my brain is screwed urghhh

---
+++ commands/General/Tools/avatar.js
@@ -0,0 +1,25 @@
+const {
+    Command
+} = require("klasa");
+const {
+    MessageEmbed
+} = require("discord.js");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            aliases: ["pfp", "profile"],
+            description: "Returns the avatar of a user in full size."
+        });
+    }
+    run(message, [member = message.member]) {
+        if (!member) {
+            return message.sendEmbed(new MessageEmbed()
+                .setColor(member.displayHexColor || 0xFFFFFF)
+                .setTitle("Here is your avatar: ")
+                .setThumbnail(message.author.displayAvatarURL({
+                    size: 2048
+                })));
+        }
+    }
+}
