98a40a5673f0da312d90d1333595dea1fea4d66e
Jacob Bond <jacobbond@Jacobs-MacBook.local>
2018-10-14 12:13:01 +0100

    fix: adcheck again

--- commands/Moderation/adcheck.js
+++ commands/Moderation/adcheck.js
@@ -10,7 +10,7 @@ module.exports = class extends Command {
         })
     }
     run(message, [channel = message.channel]) {
-        let check = message.guild.members.filter(m => m.displayName.includes('discord.gg') === m.username);
+        let check = message.guild.members.filter(m => m.displayName.includes('discord.gg'));
         message.channel.send(`\`\`\`\n${message.guild.members.map(m => m.tag).join('\n')}\n\`\`\``)
     }
 };
