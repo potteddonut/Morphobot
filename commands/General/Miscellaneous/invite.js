328005a91ac6dfc3e04112886a5ef4aad5d87abb
Morphoxeris <quartzcraftyt@gmail.com>
2018-10-20 15:01:39 +0800

    invite command enhanced hehehe

--- commands/General/Miscellaneous/invite.js
+++ commands/General/Miscellaneous/invite.js
@@ -1,21 +1,31 @@
-const { Command } = require('klasa');
+const {
+    Command
+} = require('klasa');
+const {
+    MessageEmbed
+} = require('discord.js');
 
 module.exports = class extends Command {
 
-	constructor(...args) {
-		super(...args, {
-			runIn: ['text'],
-			guarded: true,
-			description: language => language.get('COMMAND_INVITE_DESCRIPTION')
-		});
-	}
+    constructor(...args) {
+        super(...args, {
+            runIn: ['text'],
+            guarded: true,
+            description: language => language.get('COMMAND_INVITE_DESCRIPTION')
+        });
+    }
 
-	async run(message) {
-		return message.sendLocale('COMMAND_INVITE');
-	}
+    async run(message) {
+        return message.sendEmbed(new MessageEmbed()
+            .setAuthor("Inviting Morphobot", this.client.user.displayAvatarURL())
+            .setDescription(`You can invite Morphobot to your guild [here](https://discordapp.com/oauth2/authorize?client_id=499457091526721538&permissions=134220800&scope=bot).
+			If you require help regarding the bot, you can join the support guild [here](https://discord.gg/WRR2pUt).`)
+            .setColor(message.member.displayHexColor)
+            .setFooter("Morphobot").setTimestamp());
+    }
 
-	async init() {
-		if (this.client.application && !this.client.application.botPublic) this.permissionLevel = 10;
-	}
+    async init() {
+        if (this.client.application && !this.client.application.botPublic) this.permissionLevel = 10;
+    }
 
 };
