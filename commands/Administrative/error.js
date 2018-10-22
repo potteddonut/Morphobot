a6114d66c1d860770563bd73c20ec23e422cbf59
Morphoxeris <quartzcraftyt@gmail.com>
2018-10-22 16:30:35 +0800

    Minor changes to broken error features

--- commands/Administrative/error.js
+++ commands/Administrative/error.js
@@ -1,20 +1,18 @@
 const {
     Command,
-    util: {
-        codeBlock
-    }
+    util
 } = require('klasa');
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
-            permissionLevel: 10,
+            permissionLevel: 10, // dev
             aliases: ["errorcode"],
             description: "Returns details of an error via a generated error code."
         })
     }
     async run(message, [code]) {
         const x = await this.client.providers.default.get("errors", code);
-        if (!x) throw "That error code doesn't exist!";
-        return message.sendMessage(codeBlock("js", x || x.error));
+        if (!x) throw "";
+        return message.sendMessage(util.codeBlock("js", x || x.error));
     }
 }
--- events/commandError.js
+++ events/commandError.js
@@ -19,11 +19,11 @@ module.exports = class extends Event {
         if (error.message && error instanceof Error) {
             this.client.emit('wtf', `[COMMAND] ${command.path}\n${error.stack || error}`);
             const code = Date.now().toString(36);
-            await db.create('errors', code, error || error.stack);
-            return message.sendMessage(`An unexpected error occured! Please contact the developers with the error code \`${code}\``)
+            await db.create('errors', code, error.stack || error);
+            return message.sendMessage(`An unexpected error occured! Please contact the developers with the error code \`${code}\`.`)
         } else {
             message.sendMessage(error).catch(err => this.client.emit('wtf', err));
         }
-        return 0;
+        return null;
     }
 };
