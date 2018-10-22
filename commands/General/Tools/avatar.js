48239f6e316012b30677e18d415042b7b1c3c683
Morphoxeris <quartzcraftyt@gmail.com>
2018-10-22 11:56:19 +0800

    Enhanced avatar command.

--- commands/General/Tools/avatar.js
+++ commands/General/Tools/avatar.js
@@ -1,25 +1,22 @@
 const {
-	Command
+    Command
 } = require('klasa');
 const {
-	MessageEmbed
+    MessageEmbed
 } = require('discord.js');
 
 module.exports = class extends Command {
 
-	constructor(...args) {
-		super(...args, {
-			aliases: ['pfp', 'profile'],
-			description: 'Returns the avatar of a user in full size.'
-		});
-	}
-	run(message, [member = message.member]) {
-		return message.sendEmbed(new MessageEmbed()
-			.setColor(member.displayHexColor || 0xFFFFFF)
-			.setTitle('Here is your avatar: ')
-			.setImage(message.author.displayAvatarURL({
-				size: 2048
-			})));
-	}
-
+    constructor(...args) {
+        super(...args, {
+            aliases: ['pfp', 'profile'],
+            description: 'Returns the avatar of a user in full size.',
+            usage: "[user:user]"
+        });
+    }
+    run(message, [user = message.author]) {
+        return message.sendEmbed(new MessageEmbed()
+            .setAuthor(user.tag, user.displayAvatarURL(), user.displayAvatarURL())
+            .setImage(user.displayAvatarURL()));
+    }
 };
