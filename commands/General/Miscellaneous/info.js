9bb1587c87aa05d73e13fc6e241e41f5d92251df
Morphoxeris <quartzcraftyt@gmail.com>
2018-10-23 07:26:41 +0800

    Styled info command to make embed turn out nicer.

--- commands/General/Miscellaneous/info.js
+++ commands/General/Miscellaneous/info.js
@@ -18,11 +18,11 @@ module.exports = class extends Command {
 	async run(message) {
 		message.channel.sendEmbed(new MessageEmbed()
 			.setAuthor(`Morphobot is a bot written in discord.js and built on the Klasa framework.`, this.client.user.displayAvatarURL())
-			.addField("**How do I use the bot?**", `Using \`${message.guild.settings.prefix}help\` will send you a list of usable commands to you in DMs. Ensure that Morphobot has the permissions to do so. Take note that it only displays commands that you have access to. You can mention the bot to view the current prefix.`)
-			.addField("**What does Morphobot specialize in?**", `Although Morphobot does expect to turn out to be a well-designed and reliable moderation bot, we are currently trying our best to deliver to you a plethora of well-balanced features along with the bot. Do look out for updates to the bot, which often come with new features!`)
-			.addField("**Where can I receive support?**", `You can receive support from Morphobot Staff via our support server. We have an invite to the lounge that you can access via \`${message.guild.settings.prefix}invite.\``)
+			.addField("How do I use the bot?", `Using \`${message.guild.settings.prefix}help\` will send you a list of usable commands to you in DMs. Ensure that Morphobot has the permissions to do so. Take note that it only displays commands that you have access to. You can mention the bot to view the current prefix.`)
+			.addField("What does Morphobot specialize in?", `Although Morphobot does expect to turn out to be a well-designed and reliable moderation bot, we are currently trying our best to deliver to you a plethora of well-balanced features along with the bot. Do look out for updates to the bot, which often come with new features!`)
+			.addField("Where can I receive support?", `You can receive support from Morphobot Staff via our support server. We have an invite to the lounge that you can access via \`${message.guild.settings.prefix}invite.\``)
 			.setColor(message.member.displayHexColor)
-			.setFooter(`Created by Morphoxeris#1111`).setTimestamp());
+			.setFooter(`Morphobot, by Morphoxeris#1111`).setTimestamp());
 	}
 
 };
