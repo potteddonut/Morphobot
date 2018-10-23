4f8f6a05792babc2762aa5d4baccb8566a5921ed
Morphoxeris <quartzcraftyt@gmail.com>
2018-10-23 14:36:14 +0800

    Addrole command.

---
+++ commands/Moderation/addrole.js
@@ -0,0 +1,26 @@
+const {
+    Command
+} = require('klasa');
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            permissionLevel: 5,
+            aliases: ["sr", "giverole"],
+            usage: "<member:member> <role:rolename>",
+            usageDelim: " ",
+            requiredPermissions: ["MANAGE_ROLES"],
+            description: "Assigns a role."
+        });
+    }
+    async run(message, [member, role]) {
+        if (member.roles.has(role.id)) return message.send("The specified user already has that role.");
+        if (role.position >= message.member.roles.highest.position) throw "That role's position is higher than your highest role's position, thus I cannot assign the role.";
+        if (role.position >= message.guild.me.roles.highest.position) {
+            throw "That role's position is higher than or equal to my highest role, thus I cannot assign it."
+        };
+
+        return member.roles.add(role, `${message.author.tag} assigned role using the -addrole command.`)
+            .then(() => message.send(`Succesfully assigned role \`${role.name}\` to \`${member.user.tag}\``))
+            .catch(error => this.client.emit('error', error));
+    }
+};
