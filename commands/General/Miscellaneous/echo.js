9a3858dd394e0ee91c1ef5816432c3c04d30e597
Morphoxeris <quartzcraftyt@gmail.com>
2018-10-27 12:24:53 +0800

    Fixed echo.

--- commands/General/Miscellaneous/echo.js
+++ commands/General/Miscellaneous/echo.js
@@ -1,4 +1,6 @@
-const { Command } = require('klasa');
+const {
+	Command
+} = require('klasa');
 
 module.exports = class extends Command {
 
@@ -7,13 +9,16 @@ module.exports = class extends Command {
 			permissionLevel: 5,
 			runIn: ['text'],
 			description: 'Make the bot repeat / send a message.',
-			usage: '[channel:channel] <message:string>[...]',
+			usage: '<channel:channel> <message:string>[...]',
 			usageDelim: ' '
 		});
 	}
+
 	async run(message, [channel, ...text]) {
 		text = text.join(this.usageDelim);
-		if (channel.guild !== message.guild) { throw "You can't echo in other servers!"; }
+		if (channel.guild !== message.guild) {
+			throw "You can't echo in other servers!";
+		}
 		if (!channel.postable) throw "I can't echo in that channel.";
 		return channel.send(text).catch(this.client.console.error.bind(this));
 	}
