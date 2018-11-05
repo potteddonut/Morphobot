07400946fc4872f58f2b6ce7a4aafdb2864c4e55
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-05 23:37:03 +0800

    upvote cmd

---
+++ commands/General/Miscellaneous/upvote.js
@@ -0,0 +1,15 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            aliases: ['vote']
+        });
+    }
+    run(message) {
+        message.sendEmbed(new MessageEmbed()
+            .setAuthor(`Upvoting Morphobot`, this.client.user.displayAvatarURL())
+            .setDescription("You can upvote Morphobot [here](https://discordbots.org/bot/499457091526721538/vote)"));
+    }
+}
