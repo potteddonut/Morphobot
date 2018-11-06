2e38649cee2990bd0c79c8de0772ce2bf0625d29
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-06 22:45:36 +0800

    Slight changes, added yomomma command

---
+++ commands/Fun/yomomma.js
@@ -0,0 +1,17 @@
+const { Command } = require('klasa-pieces');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            aliases: ['yomoma'],
+            description: 'Outputs a yo momma joke.'
+        });
+    }
+    async run(msg) {
+        const joke = await fetch('https://api.yomomma.info')
+            .then(response => response.json())
+            .then(body => body.joke);;
+        return msg.sendMessage(`📢 ${joke}`);
+    }
+}
