e37449db7157cdd896f566f35791b919d192d24b
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-07 20:14:07 +0800

    using node-fetch

--- commands/Fun/pun.js
+++ commands/Fun/pun.js
@@ -5,8 +5,11 @@ module.exports = class extends Command {
         super(...args, { description: 'Generates a random pun.' });
     }
     async run(msg) {
-        const { body } = await fetch('https://icanhazdadjoke.com')
-            .then(result => result.json());
+        const { body } = await fetch('https://icanhazdadjoke.com', {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
         return msg.sendMessage(body.joke.length ? `Random pun: **${body.joke}**` : 'Something went wrong. Try again in a bit.')
     }
 };   
