a17b15e63c808881e1cc3d35effad560a3750c18
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-12 13:06:19 +0800

    fml

--- commands/Fun/trump.js
+++ commands/Fun/trump.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
     }
     async run(msg, [text]) {
         if (!text) return msg.responder.error('You need to provide some text for Trump to tweet!');
-        await msg.sendMessage("Making the president tweet... hold up!")
+        await msg.sendMessage("Making the president tweet... hold up!mb")
         const url = new URL('https://nekobot.xyz/api/imagegen');
         url.search = new URLSearchParams([
             ['type', 'trumptweet'],
@@ -19,6 +19,6 @@ module.exports = class extends Command {
         ]);
         const { body } = await superagent.get(url.href)
             .catch(() => msg.responder.error('An unexpected error occured with the API. Try again in a bit!'));
-        return msg.channel.sendFile(body.message);
+        return msg.sendFile(body.message);
     }
 };
