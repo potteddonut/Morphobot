fe0ec12d84117c7da12bddfcb01cd9d3e1be7455
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-15 22:43:28 +0800

    Fix my stupid arse.

--- commands/General/Tools/search.js
+++ commands/General/Tools/search.js
@@ -20,12 +20,12 @@ module.exports = class extends Command {
         const result = JSON.parse(body);
         const [topic] = result.RelatedTopics;
         if (!topic) throw "I was unable to find any results for that query.";
-        const embed = await new MessageEmbed()
+        await msg.channel.sendEmbed(new MessageEmbed()
             .setColor(msg.member.roles.highest.color)
             .setURL(topic.FirstURL)
             .setThumbnail(result.Image ? result.Image : topic.Icon.URL)
             .setTitle(result.Heading)
-            .setDescription(result.Abstract ? result.Abstract : topic.Text);
+            .setDescription(result.Abstract ? result.Abstract : topic.Text));
         return msg.responder.success(`I found the best result for your query, ${msg.author}!`, { embed: embed });
     }
 }
