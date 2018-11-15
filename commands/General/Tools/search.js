930f05179dcc174621d0594cb274f06d1e9bb458
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-15 22:48:31 +0800

    ummm

--- commands/General/Tools/search.js
+++ commands/General/Tools/search.js
@@ -20,12 +20,12 @@ module.exports = class extends Command {
         const result = JSON.parse(body);
         const [topic] = result.RelatedTopics;
         if (!topic) throw "I was unable to find any results for that query.";
-        await msg.channel.sendEmbed(new MessageEmbed()
+        await msg.responder.success(`I found the best result for your query, ${msg.author}!`);
+        return msg.channel.sendEmbed(new MessageEmbed()
             .setColor(msg.member.roles.highest.color)
             .setURL(topic.FirstURL)
             .setThumbnail(result.Image ? result.Image : topic.Icon.URL)
             .setTitle(result.Heading)
             .setDescription(result.Abstract ? result.Abstract : topic.Text));
-        return msg.responder.success(`I found the best result for your query, ${msg.author}!`, { embed: embed });
     }
 }
