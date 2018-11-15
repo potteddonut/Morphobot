6eb8b36a3268c2993d62139f61f6629a87f01912
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-15 15:43:27 +0800

    Added character limit check.

--- commands/Fun/achievement.js
+++ commands/Fun/achievement.js
@@ -9,8 +9,9 @@ module.exports = class extends Command {
         });
     }
     async run(msg, [text]) {
+        if (text.length > 22) throw msg.responder.error("Text cannot exceed 22 characters!")
         await msg.channel.send(new MessageAttachment(
             await this.client.idioticAPI.achievement(msg.author.displayAvatarURL({ format: 'png', size: 32 }), text),
             'achievement.png'));
     }
-};
+}; 
