7461c626407bf1cd567bf06d0075a17e62d25972
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-16 23:54:03 +0800

    This bug has driven lots of coders completely mad. You won't believe how it ended up being fixed.

---
+++ commands/General/Tools/define.js
@@ -0,0 +1,26 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const superagent = require('superagent');
+const baseURL = 'https://od-api.oxforddictionaries.com/api/v1/entries/en/';
+const { api_keys: { oxford_app_id, oxford_app_key } } = require(`${process.cwd()}/config`);
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            description: 'Defines a word in the Oxford Dictionary.',
+            aliases: ['def'],
+            usage: '<word:str>'
+        });
+    }
+    async run(msg, [word]) {
+        console.log(oxford_app_id, oxford_app_key);
+        const { body: lookup } = await superagent.get(baseURL + encodeURIComponent(word), { headers: { app_id: oxford_app_id, app_key: oxford_app_key } }).catch(() => null);
+        if (!lookup || !lookup.results) throw "Unknown word.";
+        const res = lookup.results[0];
+        const definitions = res.lexicalEntries.map(obj => `\`[${obj.lexicalCategory}]\` **${obj.text}** (${obj.pronunciations[0].phoneticSpelling})\n _${obj.entries[0].senses[0].definitions[0]}_`)
+        return msg.channel.sendEmbed(new MessageEmbed()
+            .setTitle(`${res.lexicalEntries.length} result${res.lexicalEntries.length !== 1 ? 's' : ''} for ${res.id}:`)
+            .setDescription(definitions.join('\n\n'))
+            .setColor(msg.guild ? msg.member.displayColor : 'RANDOM'));
+    }
+};
