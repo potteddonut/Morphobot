2e37e32e9c6cae100077a4d57ea2ddb3fa562138
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-20 18:52:05 +0800

    fix: broken help command

--- commands/General/Miscellaneous/help.js
+++ commands/General/Miscellaneous/help.js
@@ -1,3 +1,4 @@
+// Copyright (c) 2017-2018 dirigeants. All rights reserved. MIT license.
 const { Command, RichDisplay, util: { isFunction } } = require('klasa');
 const { MessageEmbed, Permissions } = require('discord.js');
 
@@ -5,6 +6,7 @@ const PERMISSIONS_RICHDISPLAY = new Permissions([Permissions.FLAGS.MANAGE_MESSAG
 const time = 1000 * 60 * 3;
 
 module.exports = class extends Command {
+
 	constructor(...args) {
 		super(...args, {
 			aliases: ['commands', 'cmd', 'cmds'],
@@ -18,40 +20,49 @@ module.exports = class extends Command {
 			return this.client.arguments.get('command').run(arg, possible, message);
 		});
 
+		// Cache the handlers
+		this.handlers = new Map();
 	}
-	async run(message, [command]) {
 
+	async run(message, [command]) {
 		if (command) {
 			return message.sendMessage([
-				`= ${command.name}`,
+				`= ${command.name} = `,
 				isFunction(command.description) ? command.description(message.language) : command.description,
 				message.language.get('COMMAND_HELP_USAGE', command.usage.fullUsage(message)),
 				message.language.get('COMMAND_HELP_EXTENDED'),
 				isFunction(command.extendedHelp) ? command.extendedHelp(message.language) : command.extendedHelp
-			], { code: 'asciidoc' })
+			], { code: 'asciidoc' });
 		}
 
 		if (!('all' in message.flags) && message.guild && message.channel.permissionsFor(this.client.user).has(PERMISSIONS_RICHDISPLAY)) {
+			// Finish the previous handler
 			const previousHandler = this.handlers.get(message.author.id);
 			if (previousHandler) previousHandler.stop();
+
 			const handler = await (await this.buildDisplay(message)).run(await message.send('Loading Commands...'), {
 				filter: (reaction, user) => user.id === message.author.id,
 				time
 			});
-			handler.on('end', () => this.handlers.delete(message.author.id));;
+			handler.on('end', () => this.handlers.delete(message.author.id));
 			this.handlers.set(message.author.id, handler);
 			return handler;
 		}
 
+		return message.author.send(await this.buildHelp(message), { split: { char: '\n' } })
+			.then(() => { if (message.channel.type !== 'dm') message.sendMessage(message.language.get('COMMAND_HELP_DM')); })
+			.catch(() => { if (message.channel.type !== 'dm') message.sendMessage(message.language.get('COMMAND_HELP_NODM')); });
 	}
 
 	async buildHelp(message) {
 		const commands = await this._fetchCommands(message);
 		const { prefix } = message.guildSettings;
+
 		const helpMessage = [];
 		for (const [category, list] of commands) {
 			helpMessage.push(`**${category} Commands**:\n`, list.map(this.formatCommand.bind(this, message, prefix, false)).join('\n'), '');
 		}
+
 		return helpMessage.join('\n');
 	}
 
@@ -67,6 +78,7 @@ module.exports = class extends Command {
 				.setDescription(list.map(this.formatCommand.bind(this, message, prefix, true)).join('\n'))
 			);
 		}
+
 		return display;
 	}
 
@@ -89,7 +101,6 @@ module.exports = class extends Command {
 		));
 
 		return commands;
-
 	}
 
 };
