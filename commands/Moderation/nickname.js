9466350a8da92f94b7aa8db897ec79d0b7b21f34
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-23 17:57:59 +0800

    Nickname command.

--- commands/Moderation/nickname.js
+++ commands/Moderation/nickname.js
@@ -16,28 +16,36 @@ module.exports = class extends Command {
 		});
 	}
 
-	// async run(message, [user = message.member, newNickName]) {
-	// 	if (!newNickName) { newNickName = '' };
-	// 	newNickName = newNickName.join(this.usageDelim);
-	// 	await user.setNickname(newNickName)
-	// 		.then(() => message.send(`Succesfully set **${user.user.tag}**'s nickname to **${newNickName}**.`))
+	// async run(msg, [user = msg.member, ...newnickname]) {
+	// 	newnickname = newnickname.join(this.usageDelim);
+	// 	if (!newnickname) user.setNickname('')
+	// 		.then(() => msg.responder.success(`Succesfully reset **${user.user.tag}**'s nickname.`))
 	// 		.catch(() => {
-	// 			throw "I can't change that user's nickname. Make sure their role is below mine.";
+	// 			throw msg.responder.error(`I can't reset the nickname. Make sure their highest role is below mine.`)
+	// 		})
+	// 	await user.setNickname(newnickname)
+	// 		.then(() => msg.responder.success(`Succesfully set **${user.user.tag}**'s nickname to **${newnickname}**.`))
+	// 		.catch(() => {
+	// 			throw "I can't change that user's nickname. Make sure their highest role is below mine."
 	// 		});
 	// }
 
-	async run(msg, [user = msg.member, ...newnickname]) {
-		newnickname = newnickname.join(this.usageDelim);
-		if (!newnickname) user.setNickname('')
-			.then(() => msg.responder.success(`Succesfully reset **${user.user.tag}**'s nickname.`))
-			.catch(() => {
-				throw msg.responder.error(`I can't reset the nickname. Make sure their highest role is below mine.`)
-			})
-		await user.setNickname(newnickname)
-			.then(() => msg.responder.success(`Succesfully set **${user.user.tag}**'s nickname to **${newnickname}**.`))
-			.catch(() => {
-				throw "I can't change that user's nickname. Make sure their highest role is below mine."
-			});
+	async run(msg, [user = msg.member, ...newNickname]) {
+		newNickname = newNickname.join(this.usageDelim);
+
+		if (!newNickname) {
+			await user.setNickname('')
+				.then(() => msg.responder.success(`Succesfully reset **${user.user.tag}**'s nickname.`))
+				.catch(() => {
+					throw msg.responder.error(`I can't reset the nickname. Make sure their highest role is below mine.`)
+				})
+		} else {
+			await user.setNickname(newNickname)
+				.then(() => msg.responder.success(`Succesfully set **${user.user.tag}**'s nickname to **${newNickname}`))
+				.catch(() => {
+					throw msg.responder.error(`I can't change that user's nickname. Make sure their highest role is below mine.`)
+				});
+		}
 	}
 
 };
