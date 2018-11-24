a64c770529da4039da3ec05c5665f5c871ed5f24
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-24 22:46:18 +0800

    Improve adcheck command.

--- commands/Moderation/adcheck.js
+++ commands/Moderation/adcheck.js
@@ -1,6 +1,5 @@
-const {
-	Command
-} = require('klasa');
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
 module.exports = class extends Command {
 
 	constructor(...args) {
@@ -12,7 +11,13 @@ module.exports = class extends Command {
 	}
 	run(message) {
 		const found = message.guild.members.filter(member => member.displayName.includes('discord.gg'));
-		message.channel.send(`\`\`\`\n${found.map(member => member.user.tag).join('\n')}\n\`\`\``);
+		if (!found) return msg.responder.error('There are no members with any advertisements in their names.');
+		// message.channel.send(`\`\`\`\n${found.map(member => member.user.tag).join('\n')}\n\`\`\``);
+		msg.sendEmbed(new MessageEmbed()
+			.setTitle('Server Members with invites in their name -')
+			.setDescription(found.map(member => member.user.tag).join('\n'))
+			.setFooter('Morphobot Adcheck').setTimestamp()
+			.setColor(message.member.displayHexColor));
 	}
 
 };
