41bbd6683d46011a1fd4961ec282153b699f3f54
Morphoxeris <quartzcraftyt@gmail.com>
2018-11-24 22:51:52 +0800

    Tidied things up.

--- commands/Moderation/adcheck.js
+++ commands/Moderation/adcheck.js
@@ -11,8 +11,7 @@ module.exports = class extends Command {
 	}
 	run(message) {
 		const found = message.guild.members.filter(member => member.displayName.includes('discord.gg'));
-		if (!found) return msg.responder.error('There are no members with any advertisements in their names.');
-		// message.channel.send(`\`\`\`\n${found.map(member => member.user.tag).join('\n')}\n\`\`\``);
+		if (!found.size) return message.responder.error('There are no members with invites in their playing status.')
 		message.sendEmbed(new MessageEmbed()
 			.setTitle('Server Members with invites in their name -')
 			.setDescription(found.map(member => member.user.tag).join('\n'))
