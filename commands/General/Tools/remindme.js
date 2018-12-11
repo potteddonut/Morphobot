ed500e7a6a20e3dfb4f51b88907c045ad2182bc1
Morphoxeris <quartzcraftyt@gmail.com>
2018-12-11 15:36:06 +0800

    Changed remindme command usage, removed unnecessary comma.

--- commands/General/Tools/remindme.js
+++ commands/General/Tools/remindme.js
@@ -8,7 +8,7 @@ module.exports = class extends Command {
 			description: 'Creates a new reminder.',
 			extendedHelp: 'For example, to set a reminder that goes off in 10 minutes, you will do - mb!remindme 10m, do this.',
 			usage: '<when:time> <text:...str>',
-			usageDelim: ', '
+			usageDelim: ' '
 		});
 	}
 	async run(message, [when, text]) {
