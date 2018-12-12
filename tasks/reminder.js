19d36c0a964f5f3c32ea0a198491fcb3e9797d65
Zach <31350835+Morphoxeris@users.noreply.github.com>
2018-12-12 23:27:09 +0800

    Attempt to change reminder to DMs.

--- tasks/reminder.js
+++ tasks/reminder.js
@@ -8,8 +8,8 @@ module.exports = class extends Task {
 		user,
 		text
 	}) {
-		const _channel = this.client.channels.get(channel);
-		if (_channel) await _channel.send(`<@${user}>, You wanted me to remind you: ${text}`);
+		const _user = message.guild.users.get(user);
+		if (_user) await _user.send(`<@${user}>, You wanted me to remind you: ${text}`);
 	}
 
 };
