11a82a7737e34a69781947a8a3f97b559cc51ace
Morphoxeris <quartzcraftyt@gmail.com>
2018-12-16 23:01:47 +0800

    Improve exec.js

--- commands/Administrative/exec.js
+++ commands/Administrative/exec.js
@@ -1,24 +1,28 @@
+// Copyright (c) 2017-2018 dirigeants. All rights reserved. MIT license.
 const { Command, util: { exec, codeBlock } } = require('klasa');
+
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			alises: ['execute', 'console'],
-			description: 'Executes commands in the terminal. Use with extreme caution!',
-			// eslint-disable-next-line max-len
-			extendedHelp: 'The exec command (better known as execute) will run any form of string provided in the direct console, which will affect the bot. Beware - do not shove random text or commands you\'re unaware of up this commands ass!',
+			aliases: ['execute'],
+			description: 'Execute commands in the terminal, use with EXTREME CAUTION.',
 			guarded: true,
 			permissionLevel: 10,
-			usage: '<expression:string>'
+			usage: '<expression:string>',
+			extendedHelp: 'Times out in 60 seconds by default. This can be changed with --timeout=TIME_IN_MILLISECONDS'
 		});
 	}
+
 	async run(msg, [input]) {
+		await msg.sendMessage('Executing your command...');
+
 		const result = await exec(input, { timeout: 'timeout' in msg.flags ? Number(msg.flags.timeout) : 60000 })
 			.catch(error => ({ stdout: null, stderr: error }));
 		const output = result.stdout ? `**\`OUTPUT\`**${codeBlock('prolog', result.stdout)}` : '';
 		const outerr = result.stderr ? `**\`ERROR\`**${codeBlock('prolog', result.stderr)}` : '';
 
-		return msg.sendMessage([output, outerr].join('\n'));
+		return msg.sendMessage([output, outerr].join('\n') || 'Done. There was no output to stdout or stderr.');
 	}
 
 };
